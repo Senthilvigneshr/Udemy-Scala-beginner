b7601b4ffbed5a73fb61db321ec1933d9861919a
Senthilvigneshr <senthilvigneshr@gmail.com>
2022-06-18 18:40:38 +0530

    ValVarTypes

--- src/main/scala/Playground/ValuesVariablesTypes.scala
+++ src/main/scala/Playground/ValuesVariablesTypes.scala
@@ -3,7 +3,19 @@ package Playground
 object ValuesVariablesTypes extends App{
 
     val x:Int = 42
+    //val x = 42
     println(x)
+
+    //VALS ARE IMMUTABLE
+
+    // COMPILER can infer types
+
+    val aString : String = "hello"
+    val aBoolean : Boolean = true
+    val aChar : Char = 'a'
+
+    var aVariable : Int = 4
+     aVariable = 5 //side effects
   }
 
 
