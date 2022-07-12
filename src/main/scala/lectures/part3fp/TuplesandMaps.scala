6d8143c279d7c287d277969d8823b8c075215052
Senthilvigneshr <senthilvigneshr@gmail.com>
2022-07-12 21:30:06 +0530

    adding exercisesforTuplesandMaps

--- src/main/scala/lectures/part3fp/TuplesandMaps.scala
+++ src/main/scala/lectures/part3fp/TuplesandMaps.scala
@@ -40,6 +40,53 @@ object TuplesandMaps extends App {
   val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
   println(names.groupBy(name => name.charAt(0)))
 
+  /*
+    1. Whta would happen if i have 2 orinial entries "Jim" -> 555 and JIM -> 999 ??
 
+    2. Overly simpilfied social network based on maps
+      Person = String
+      - add a person to a network
+      - remove
+      - friend (mutual)
+      - unfriend
 
+      - number of friends of a person
+      - person with most freiends
+      - how many people have no friends
+      - if there is a social connection between two people(direct or not)
+    */
+
+    def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+      network + (person -> Set())
+    }
+
+  def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+      val friendsA = network(a)
+      val friendsB = network(b)
+
+      network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+    }
+
+    def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+      val friendsA = network(a)
+      val friendsB = network(b)
+
+      network + (a -> (friendsA - b)) + (b -> (friendsB - a))
+    }
+
+    def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+      def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+        if (friends.isEmpty) networkAcc
+        else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
+        }
+        val unfriended = removeAux(network(person), network)
+        unfriended - person
+    }
+
+      val empty: Map[String, Set[String]] = Map()
+      val network = add(add(empty, "Bob"), "Mary")
+      println(network)
+      println(friend(network, "Bob", "Mary"))
+      println(unfriend(friend(network, "Bob", "Mary"), "Bob", "Mary"))
+      println(remove(friend(network, "Bob", "Mary"), "Bob"))
 }
